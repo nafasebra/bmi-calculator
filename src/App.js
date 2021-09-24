b4309b3eca7ac939bf1aa5c5fd2d1ea8c5b9ce10
Nafiseh Ebrahimi <ebranafas@gmail.com>
2021-09-25 00:05:23 +0330

    add initial style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+    return (
+        <div className="app">
+            <h2 className="app-name">BMI Calculator</h2>
+        </div>
+    )
 }
 
 export default App;
