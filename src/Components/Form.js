2d558930d3eafd263d7d9e9153cc753ade0f57ae
unknown <ebranafas@gmail.com>
2021-10-24 19:37:59 +0330

    add pupup status open

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+// import component
+import Popup from './Popup';
 
 // styles of component
 import './Form.css';
 
 function Form() {
+
+    const [openPopup, setOpenPopup] = useState(false);
+    
+    // const calculate = () => {
+    //     setOpenPopup(true);
+    // }
+
     return (
         <div className="form-container">
             <div>
-                <p>Input your Weight and Height and get your BMI.</p>
+                <p>Input uarnyour Weight and Height and get your BMI.</p>
                 <div className="form-control">
                     <input type="text" required id="height"/>
                     <label htmlFor="height">your Height (cm)</label>
@@ -16,11 +26,13 @@ function Form() {
                     <input type="text" required id="weight"/>
                     <label htmlFor="weight">your Weight (kg)</label>
                 </div>
-                <button>
+                <button className="btn" onClick={() => setOpenPopup(true)}>
                     <span>CALCULATE</span>
-                    <svg width="15px" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    <svg width="15px" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                 </button>
             </div>
+
+            <Popup status={openPopup} setFalse={() => setOpenPopup(false)} />
         </div>
     )
 }
--- src/Components/Popup.js
+++ src/Components/Popup.js
@@ -1,23 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // import styles of popup
 import './Popup.css';
 
-function Popup() {
+function Popup({ status, setFalse }) {
 
     return (
-        <div className="popup-container">
-            <div className="popup">
-                <div className="popup-header">
-                    <h4>your result:</h4>
-                    <svg width="20px" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        <>
+            {
+                status ?
+                <div className="popup-container">
+                    <div className="popup">
+                        <div className="popup-header">
+                            <h4>your result:</h4>
+                            <button onClick={() => setFalse()}>
+                                <svg width="20px" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                            </button>
+                        </div>
+                        <div className="popup-body">
+                            <h1>22.5</h1>
+                            <h3>Good weight</h3>
+                        </div>
+                    </div>
                 </div>
-                <div className="popup-body">
-                    <h1>22.5</h1>
-                    <h3>Good weight</h3>
-                </div>
-            </div>
-        </div>
+                :
+                null
+            }
+        </>
     )
 }
 
